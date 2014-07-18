27b9a5c51ca18cd3af5262b65ee946aa0da6aab2
kaiwong <WK  work>
2014-07-18 10:49:02 +0800

    My first test
    just learning

---
+++ firsttest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  firsttest
+//
+//  Created by wang kai on 14-7-17.
+//  Copyright (c) 2014年 wang kai. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
---
+++ firsttestTests/firsttestTests.swift
@@ -0,0 +1,36 @@
+//
+//  firsttestTests.swift
+//  firsttestTests
+//
+//  Created by wang kai on 14-7-17.
+//  Copyright (c) 2014年 wang kai. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class firsttestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
